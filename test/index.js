074c8617e80afbf18693eaf7bcc824c777308c28
Ross Khanas <rtkhanas@users.noreply.github.com>
2017-02-27 17:04:36 +0900

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,7 +1,20 @@
 import assert from 'assert';
+import iCloudSession, { requestICloudData } from '../src';
 
-describe('#indexOf()', () => {
-  it('should return -1 when the value is not present', () => {
-    assert.equal(-1, [1, 2, 3].indexOf(4));
+describe('typeof', () => {
+  it('iCloudSession is an object', () => {
+    assert.equal(typeof iCloudSession === 'object', true);
+  });
+  it('iCloudSession.login is a function', () => {
+    assert.equal(typeof iCloudSession.login === 'function', true);
+  });
+  it('iCloudSession.loadSessionFile is a function', () => {
+    assert.equal(typeof iCloudSession.loadSessionFile === 'function', true);
+  });
+  it('iCloudSession.saveSession is a function', () => {
+    assert.equal(typeof iCloudSession.saveSession === 'function', true);
+  });
+  it('requestICloudData is a function', () => {
+    assert.equal(typeof requestICloudData === 'function', true);
   });
 });
